e7c25e4125f0ce865c6d5b7e7d87090cd5191f5f
Mariu <dumitrumarius@dumitrus-mbp.home>
2019-12-28 22:12:24 +0000

    created registration view to allow users to register

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from accounts.forms import UserLoginForm, UserRegistrationForm
 
 # Create your views here.
+def index(request):
+	return render(request, 'index.html')
+
+def registration(request):
+    """Render the regiastration page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+        
+    if request.method == "POST":
+        registration_form = UserRegistrationForm(request.POST)
+        
+        if registration_form.is_valid():
+            registration_form.save()
+            
+            user = auth.authenticate(username=request.POST['username'],
+                                    password=request.POST['password1'])
+            
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully registred")
+                return redirect(reverse('index'))
+                
+            else:
+                messages.error(request, "Unable to register your account at this time")
+    
+    else:
+        registration_form = UserRegistrationForm()
+        
+    return render(request, 'registration.html', {
+        "registration_form": registration_form})
