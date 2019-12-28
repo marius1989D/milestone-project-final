3666681bbba6b733a0eb8638a45bc8321e031254
Mariu <dumitrumarius@dumitrus-mbp.home>
2019-12-28 20:39:43 +0000

    fixed bug with pagination not working

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post 
+from .models import Post, Comment 
 
 # Register your models here.
 
@@ -10,3 +10,13 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(Post, PostAdmin)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ('name', 'body', 'post', 'created_on', 'active')
+	list_filter = ('active', 'created_on')
+	search_fields = ('name', 'email', 'body')
+	actions = ['approve_comments']
+
+	def approve_comments(self, request, queryset):
+		queryset.update(active=True)
