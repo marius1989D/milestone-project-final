65c5dc921d6999469f9d028366fbf1a71be88dc7
Mariu <dumitrumarius@dumitrus-mbp.home>
2019-12-29 13:40:15 +0000

    created requirements.txt and moved index file to one level above

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, get_object_or_404
 
 class PostList(generic.ListView):
 	queryset = Post.objects.filter(status=1).order_by('created_on')
-	template_name = 'blog/index.html'
+	template_name = 'index.html'
 	paginate_by = 3
 
 def post_detail(request, slug):
