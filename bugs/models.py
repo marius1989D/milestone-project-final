69b86c651a7063f4f44790fe4f8c26ef3b5838ca
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-01-01 16:56:14 +0000

    corrected typo and syntax error on models

--- bugs/models.py
+++ bugs/models.py
@@ -3,11 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
-CHOICES = [
-	(Received, 'Received'),
-	(In progress, 'In progress'),
-	(Done, 'Done')
-]
+CHOICES = (
+	(0, 'Received'),
+	(1, 'In progress'),
+	(2, 'Done')
+)
 
 class Bug(models.Model):
 
@@ -19,7 +19,7 @@ class Bug(models.Model):
 	author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bugs_posts')
 	content = models.TextField()
 	created_on = models.DateTimeField(auto_now_add=True)
-	status = models.IntegerField(choices=STATUS, default=Received)
+	status = models.IntegerField(choices=CHOICES, default=0)
 	views = models.IntegerField(default=0)
 
 	class Meta:
