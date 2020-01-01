0f157a44ab91219c96b46ac0a35141735320f1d1
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-01-01 20:16:02 +0000

    fiex bug with comment on a bug post

--- bugs/views.py
+++ bugs/views.py
@@ -27,7 +27,7 @@ def bug_detail(request, pk):
 			# Create Comment object but don't save to database yet
 			new_comment = comment_form.save(commit=False)
 			# Assign the current post to the comment
-			new_comment.post = post
+			new_comment.bug = bug
 			# Save the comment to the database
 			new_comment.save()
 	else:
