6d8fa836b6a58d2b2d3414691de1891741459a57
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-01-01 17:24:12 +0000

    fixed issue with bugs list not rendering on the template

--- bugs/urls.py
+++ bugs/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.BugList.as_view(), name='bugs_home')
+	path('', views.BugList.as_view(), name='bug_list')
 ]
--- bugs/views.py
+++ bugs/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Bug
 
 # Create your views here.
+class BugList(generic.ListView):
+	
+
+	"""
+	This view will list all bugs 
+	"""
+	queryset = Bug.objects.order_by('created_on').all()
+	template_name = 'bugs/bugs.html'
+	paginate_by = 3
+
+	
