10f2773d6a5a2781bd352e092a19ecbf9e9450a3
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-01-01 20:04:28 +0000

    added form for comment on the bugs

--- bugs/forms.py
+++ bugs/forms.py
@@ -1,8 +1,12 @@
 from django import forms
-from .models import Bug
+from .models import Bug, Comment
 
 class BugPostForm(forms.ModelForm):
 	class Meta:
 		model = Bug
 		fields = ('title', 'content')
 
+class BugCommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
