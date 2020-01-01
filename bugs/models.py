375db599ec5e805f6710d60e91d4dc4c99c17543
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-01-01 20:02:51 +0000

    added model for comment on the bugs

--- bugs/models.py
+++ bugs/models.py
@@ -16,7 +16,7 @@ class Bug(models.Model):
 	"""
 
 	title = models.CharField(max_length=200)
-	author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bugs_posts')
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
 	content = models.TextField()
 	created_on = models.DateTimeField(auto_now_add=True)
 	status = models.IntegerField(choices=CHOICES, default=0)
@@ -28,6 +28,19 @@ class Bug(models.Model):
 	def __str__(self):
 		return self.title
 	
+class Comment(models.Model):
+	bug = models.ForeignKey(Bug, on_delete=models.CASCADE, related_name='comments')
+	name = models.CharField(max_length=80)
+	email = models.EmailField()
+	body = models.TextField()
+	created_on = models.DateTimeField(auto_now_add=True)
+	active = models.BooleanField(default=False)
+
+	class Meta:
+		ordering = ['created_on']
+
+	def __str__(self):
+		return 'Comment {} by {}'.format(self.body, self.name)
 
 
 
