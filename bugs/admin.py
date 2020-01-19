142831503ad68d52870da90651d0886ff801ad54
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-01-19 15:19:19 +0000

    customized bugs reports on admin pannel

--- bugs/admin.py
+++ bugs/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Bug, Comment
 
-# Register your models here.
-admin.site.register(Bug)
+class BugAdmin(admin.ModelAdmin):
+	list_display = ('title', 'author', 'status')
+	list_filter = ('status', 'created_on')
+	search_fields = ('author', 'title')
+	list_editable = ('status',)
+	date_hierarchy = ('created_on')
+
+
+
+
+admin.site.register(Bug, BugAdmin)
 admin.site.register(Comment)
