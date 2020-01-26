7bd7a6ad7da1d92e50a1481d048bcf770e5e5eaa
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-01-26 16:39:34 +0000

    started checkout app

--- bug_issue_tracker/settings.py
+++ bug_issue_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import env
 from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
     'bugs',
     'features',
     'cart',
+    'checkout',
 
 ]
 
@@ -155,5 +157,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'creion.m@gmail.com'
-EMAIL_HOST_PASSWORD = 'marius123B'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
