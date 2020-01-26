23e9af41ceba85bca81ef54e2b6bea76096e9175
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-01-26 17:09:31 +0000

    corected typo

--- checkout/admin.py
+++ checkout/admin.py
@@ -4,7 +4,7 @@ from .models import Order, OrderLineItem
 class OrderLineAdminInline(admin.TabularInline):
 	model = OrderLineItem
 
-class OrderAdmin(admn.ModelAdmin):
+class OrderAdmin(admin.ModelAdmin):
 	inlines = (OrderLineAdminInline, )
 
 admin.site.register(Order, OrderAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -3,20 +3,20 @@ from features.models import Feature
 from django.utils import timezone
 
 class Order(models.Model):
-	full_name = models.Charfield(max_length=50, blank=False)
-	phone_number = models.Charfield(max_length=20, blank=False)
-	country = models.Charfield(max_length=40, blank=False)
-	post_code = models.Charfield(max_length=20, blank=False)
-	town_or_city = models.Charfield(max_length=40, blank=True)
-	street_address = models.Charfield(max_length=40, blank=False)
+	full_name = models.CharField(max_length=50, blank=False)
+	phone_number = models.CharField(max_length=20, blank=False)
+	country = models.CharField(max_length=40, blank=False)
+	post_code = models.CharField(max_length=20, blank=False)
+	town_or_city = models.CharField(max_length=40, blank=True)
+	street_address = models.CharField(max_length=40, blank=False)
 	date = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
 
 class OrderLineItem(models.Model):
-	order = models.ForeignKey(Order, null=False)
-	product = models.ForeignKey(Feature, null=False)
+	order = models.ForeignKey(Order, null=False, on_delete=models.CASCADE)
+	product = models.ForeignKey(Feature, null=False, on_delete=models.CASCADE)
 	quantity = models.IntegerField(blank=False)
 
 	def __str__(self):
