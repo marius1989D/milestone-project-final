08dc62bf5637ebec4f8835be57b309243a5147dd
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-02-04 11:27:14 +0000

    fixed bug with payments not getting token

--- checkout/forms.py
+++ checkout/forms.py
@@ -2,7 +2,13 @@ from django import forms
 from .models import Order
 
 class MakePaymentForm(forms.Form):
+	"""cardNumber = forms.CharField(label='Credit card number', required=False)
+	cardExpiry = forms.IntegerField(default=0)
+	cardCvc = forms.IntegerField(default=0)"""
 
+
+
+"""
 	MONTH_CHOICES = [(i, i) for i in range(1, 12)]
 	YEAR_CHOICES = [(i, i) for i in range(2019, 2040)]
 
@@ -11,7 +17,7 @@ class MakePaymentForm(forms.Form):
 	expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False)
 	expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
 	stripe_id = forms.CharField(widget=forms.HiddenInput)
-
+"""
 class OrderForm(forms.ModelForm):
 	class Meta:
 		model = Order
--- static/js/stripe.js
+++ static/js/stripe.js
@@ -1,32 +1,90 @@
-$(function() {
-    $("#payment-form").submit(function() {
-        var form = this;
-        var card = {
-            number: $("#id_credit_card_number").val(),
-            expMonth: $("#id_expiry_month").val(),
-            expYear: $("#id_expiry_year").val(),
-            cvc: $("#id_cvv").val()
-        };
-    
-    Stripe.createToken(card, function(status, response) {
-        if (status === 200) {
-            $("#credit-card-errors").hide();
-            $("#id_stripe_id").val(response.id);
+// Create a Stripe client.
 
-            // Prevent the credit card details from being submitted
-            // to our server
-            $("#id_credit_card_number").removeAttr('name');
-            $("#id_cvv").removeAttr('name');
-            $("#id_expiry_month").removeAttr('name');
-            $("#id_expiry_year").removeAttr('name');
+var stripe = Stripe('pk_test_0AlIWPFmuv4xQKqInu9ZXGjs00wE5LMRcy');
 
-            form.submit();
-        } else {
-            $("#stripe-error-message").text(response.error.message);
-            $("#credit-card-errors").show();
-            $("#validate_card_btn").attr("disabled", false);
-        }
+  // Create an instance of Elements.
+  var elements = stripe.elements();
+
+  // Custom styling can be passed to options when creating an Element.
+  // (Note that this demo uses a wider set of styles than the guide below.)
+  var style = {
+    base: {
+      color: '#32325d',
+      lineHeight: '18px',
+      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+      fontSmoothing: 'antialiased',
+      fontSize: '16px',
+      '::placeholder': {
+        color: '#aab7c4'
+      }
+    },
+    invalid: {
+      color: '#fa755a',
+      iconColor: '#fa755a'
+    }
+  };
+
+  // Create an instance of the card Element.
+  var card = elements.create('card', {
+  	classes : {
+  		base : "form-control",
+  		invalid : "error"
+  	}
+  });
+  
+
+  // Add an instance of the card Element into the `card-element` <div>.
+  card.mount('#card-element');
+  
+
+  // Handle real-time validation errors from the card Element.
+  card.addEventListener('change', function(event) {
+    var displayError = document.getElementById('card-errors');
+    if (event.error) {
+      displayError.textContent = event.error.message;
+    } else {
+      displayError.textContent = '';
+    }
+  });
+
+  // Handle form submission.
+  var form = document.getElementById('payment-form');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    stripe.createToken(card).then(function(result) {
+      if (result.error) {
+        // Inform the user if there was an error.
+        var errorElement = document.getElementById('card-errors');
+        errorElement.textContent = result.error.message;
+      } else {
+        // Send the token to your server.
+        stripeTokenHandler(result.token);
+      }
     });
-    return false;
-    });
-});
+
+  });
+
+  var successElement = document.getElementById('stripe-token-handler');
+  document.querySelector('.wrapper').addEventListener('click', function() {
+    successElement.className = 'is-hidden';
+  });
+
+  function stripeTokenHandler(token) {
+    successElement.className = '';
+    successElement.querySelector('.token').textContent = token.id;
+    // Insert the token ID into the form so it gets submitted to the server
+    var form = document.getElementById('payment-form');
+    var hiddenInput = document.createElement('input');
+    hiddenInput.setAttribute('type', 'hidden');
+    hiddenInput.setAttribute('name', 'stripeToken');
+    hiddenInput.setAttribute('value', token.id);
+    form.appendChild(hiddenInput);
+
+    // Submit the form
+    form.submit();
+  }
+
+
+
+
