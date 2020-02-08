ee2226fc3db723605e76e49e4440c52bd78a663a
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-02-08 22:36:40 +0000

    detailed blog app for better UX

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,10 @@ from .models import Post
 from .forms import CommentForm
 from django.shortcuts import render, get_object_or_404
 
+
 class PostList(generic.ListView):
-	queryset = Post.objects.filter(status=1).order_by('-created_on')
+	queryset = Post.objects.filter(status=1).order_by('-created_on').all()
 	template_name = 'post_list.html'
-	paginate_by = 3
 
 def post_detail(request, slug):
     template_name = 'blog/post_detail.html'
