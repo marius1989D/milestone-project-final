bc09d7c2b20d8ce97b4f842314a3aed225025a4b
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-02-16 01:30:09 +0000

    fiexd error with env.py

--- bug_issue_tracker/settings.py
+++ bug_issue_tracker/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import env
+
 from django.contrib.messages import constants as messages
+
+if os.path.exists('env.py'):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
