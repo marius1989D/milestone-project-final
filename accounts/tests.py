772945edc0b04e8bf951bb587fa21ced8ebe43c4
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-02-16 00:43:40 +0000

    added tests for login and register forms

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .forms import UserLoginForm, UserRegistrationForm
 
-# Create your tests here.
+class TestAccountsForms(TestCase):
+	"""Tests for login and registration forms"""
+	def test_login_form(self):
+		form = UserLoginForm({
+			'username_or_email': 'abcd',
+			'password': 'marius123A'
+		})
+		print(form.errors)
+		self.assertTrue(form.is_valid())
+
+	def test_registration_form(self):
+		form = UserRegistrationForm({
+			'username': 'abcd',
+			'email': 'abcd@789.ro',
+			'password1': 'marius123A',
+			'password2': 'marius123A'
+		})
+		self.assertTrue(form.is_valid())
+
+	
