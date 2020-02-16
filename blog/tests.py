5e837fb9a67ef45e675754f9e8e3647a8b20b73e
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-02-16 00:56:06 +0000

    added tests for blog app

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .forms import CommentForm
 
-# Create your tests here.
+class TestBlogCommentForm(TestCase):
+
+	"""Test blog-post comment form"""
+	def test_comment_form(self):
+		form = CommentForm({
+			'name': 'abcd', 
+			'email': 'abcd@789.ro', 
+			'body': 'comment'
+		})
+		self.assertTrue(form.is_valid())
+
+
+	"""Test for blog-post views"""
+	def test_blog_posts_home_page(self):
+		page = self.client.get("/blog/")
+		self.assertEqual(page.status_code, 200)
+		self.assertTemplateUsed(page, "blog/post_list.html")
