7c8d32c40b6bb496b81ac60e6a5c2b5e03ef467c
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-02-22 13:03:53 +0000

    added login_required for cart views

--- cart/views.py
+++ cart/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
+@login_required()
 def view_cart(request):
 	"""
 	Renders the cart content page
 	"""
 	return render(request, "cart/cart.html")
 
+@login_required()
 def add_to_cart(request, id):
     """Add a quantity of the specified product to the cart"""
     quantity = 1
@@ -21,7 +24,7 @@ def add_to_cart(request, id):
     request.session['cart'] = cart
     return redirect(reverse('cart:view_cart'))
 
-
+@login_required()
 def adjust_cart(request, id):
     """
     Adjust the quantity of the specified product to the specified
