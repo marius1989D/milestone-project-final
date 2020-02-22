284c46307c41d77210bea39931de1209aef2973d
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-02-22 11:00:02 +0000

    added tests for bugs app

--- bugs/tests.py
+++ bugs/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .forms import BugPostForm, BugCommentForm
 
-# Create your tests here.
+class TestBugForms(TestCase):
+	"""Test for Bug post forms"""
+	def test_bug_form(self):
+		form = BugPostForm({
+			'title': 'abcd',
+			'content': 'defghj'
+		})
+		print(form.errors)
+		self.assertTrue(form.is_valid())
+
+	"""Test for Bugs list view"""
+	def test_get_bugs_page(self):
+		page = self.client.get("/bugs/bugs_list/")
+		self.assertEqual(page.status_code, 200)
+		self.assertTemplateUsed(page, "bugs/bugs.html")
