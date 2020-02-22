b9a479f2a26ebfbff8aff7b85a041e4a823ceba9
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-02-22 21:33:20 +0000

    settings

--- bug_issue_tracker/settings.py
+++ bug_issue_tracker/settings.py
@@ -161,11 +161,13 @@ MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-AWS_S3_OBJECT_PARAMETERS = {
-    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-    'CacheControl': 'max-age=94608000'
-}
- 
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+
 AWS_S3_REGION_NAME = 'eu-west-1'
 
 
@@ -174,25 +176,23 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = 'ci-final-pj'
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000'
+}
 
-AWS_STATIC_LOCATION = 'static'
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'bug_issue_tracker.storage_backends.StaticStorage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
+DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage' 
 
-#STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIAFILES_LOCATION = 'media/img'
 DEFAULT_FILE_STORAGE = 'bug_issue_tracker.storage_backends.PublicMediaStorage' 
-
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
