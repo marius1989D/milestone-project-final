855be8ae795ad8ddf216e0b6912432baebc1204d
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-02-22 14:59:40 +0000

    added tests for checkout app

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TestCheckoutViews(TestCase):
+	""" 
+	Test the checkout view
+	"""
+	def test_cart_views_require_user_logged_in(self):
+		checkout_page = self.client.get('/checkout/')
+
+		self.assertEqual(checkout_page.status_code, 302)
+		self.assertRedirects(checkout_page, '/accounts/login/?next=/checkout/')
