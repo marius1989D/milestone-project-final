5aa0b43e9c3f1db58b13ee3b84e16ab1c8fbe48f
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-02-22 11:03:28 +0000

    added tests for features app

--- features/tests.py
+++ features/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .forms import FeaturePostForm, FeatureCommentForm
 
-# Create your tests here.
+class TestFeatureForms(TestCase):
+	"""Test for Feature post forms"""
+	def test_bug_form(self):
+		form = FeaturePostForm({
+			'title': 'abcd',
+			'content': 'defghj'
+		})
+		print(form.errors)
+		self.assertTrue(form.is_valid())
+
+	"""Test for Features list view"""
+	def test_get_features_page(self):
+		page = self.client.get("/features/features_list/")
+		self.assertEqual(page.status_code, 200)
+		self.assertTemplateUsed(page, "features/features.html")
