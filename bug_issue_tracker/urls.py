86d4d61bc29d6d607d89a612007aab8a8a0fdf56
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-02-22 18:47:42 +0000

    ...

--- bug_issue_tracker/urls.py
+++ bug_issue_tracker/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from accounts.views import index
+from django.conf import settings
+from django.conf.urls.static import statics
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +28,4 @@ urlpatterns = [
     path('features/', include('features.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
